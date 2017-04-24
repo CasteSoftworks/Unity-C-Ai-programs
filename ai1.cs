bb02ab963b093fd26303df8e7c28edfbc8ff33f8
CasteSoftworks <giacomo.castellana01@gmail.com>
2017-04-24 19:07:30 +0200

    For zombie-like enemy
    just follows you using the nav agent

---
+++ ai1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ai1 : MonoBehaviour {
+
+    public Transform ia2;
+    public float distanzaia1;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float distanza = Vector3.Distance(transform.position, ia2.transform.position);
+        if(distanza <= distanzaia1)
+        {
+            transform.LookAt(ia2);
+            agent.SetDestination(ia2.position);
+
+        }
+
+	}
+}
